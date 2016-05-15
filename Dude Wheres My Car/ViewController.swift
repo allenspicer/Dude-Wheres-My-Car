da37bb48a1916661f30253e5f7914c1a6fbe66e1
Allen Spicer <allenspicer@gmail.com>
2016-05-15 18:59:21 -0400

    Working Nav Bar

--- Dude Wheres My Car/ViewController.swift
+++ Dude Wheres My Car/ViewController.swift
@@ -48,7 +48,20 @@ class ViewController: UIViewController {
         
     }
 
+// MARK: - location manager to authorize user location for Maps app
+        var locationManager = CLLocationManager()
+        func checkLocationAuthorizationStatus() {
+            if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+                mapView.showsUserLocation = true
+            } else {
+                locationManager.requestWhenInUseAuthorization()
+            }
+        }
 
-
+        override func viewDidAppear(animated: Bool) {
+            super.viewDidAppear(animated)
+            checkLocationAuthorizationStatus()
+        }
+    
+    
 }
-
