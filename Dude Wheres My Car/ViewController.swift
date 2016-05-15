ff59e04be660f2c0574c3638d79e7ffdccf96a8f
Allen Spicer <allenspicer@gmail.com>
2016-05-15 17:53:59 -0400

    Update

--- Dude Wheres My Car/ViewController.swift
+++ Dude Wheres My Car/ViewController.swift
@@ -14,7 +14,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set map center Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        //define a distance to be applied below
+        let regionRadius: CLLocationDistance = 1000
+        //define a function called centermap on location which takes a location...
+        func centerMapOnLocation(location: CLLocation) {
+            //...and creates a coordinate region by applying the above distance to an east-west radius and north-south
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                      regionRadius * 2.0,
+                                                                      regionRadius * 2.0)
+            //display the region
+            mapView.setRegion(coordinateRegion, animated: true)
+            }
+
     }
 
 
